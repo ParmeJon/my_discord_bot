a6c11ce9484417603dd01078c830ca58587a10a9
github username <jonathansychan@gmail.com>
2019-06-02 12:49:54 -0400

    adds website function

--- index.js
+++ index.js
@@ -3,14 +3,27 @@ const { token } = require('./config.json')
 const bot = new Discord.Client();
 
 const discordToken = token
+const PREFIX = '!';
+
+
 
 bot.on('ready', () => {
   console.log('This bot is online!')
 })
 
-bot.on('message', msg => {
-  if(msg.content === "HELLO") {
-    msg.reply('Hello Friend!');
+bot.on('message', message => {
+  // if(msg.content === "HELLO") {
+  //   msg.reply('Hello Friend!');
+  // }
+  let args = message.content.substring(PREFIX.length).split(" ");
+  switch(args[0]){
+    case 'ping':
+      // message.reply('pong');
+      message.channel.send('pong')
+      break;
+    case 'website':
+      message.channel.send('https://www.youtube.com/jonsyvids')
+      break;
   }
 })
 
